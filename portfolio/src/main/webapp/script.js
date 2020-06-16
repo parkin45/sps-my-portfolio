9d914bd1828932c1d9be09750ab6f28ffe109071
Anna Parkinson <parkin45@msu.edu>
2020-06-16 00:10:54 -0400

    Part 4 Comments appear on html

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -28,43 +28,6 @@ function addRandomGreeting() {
 }
 
 /**
- * Fetches a random quote from the server and adds it to the DOM.
- */
-function getRandomQuote() {
-  console.log('Fetching a random quote of a female Comp Sci legend.');
-
-  // The fetch() function returns a Promise because the request is asynchronous.
-  const responsePromise = fetch('/data');
-
-  // When the request is complete, pass the response into handleResponse().
-  responsePromise.then(handleResponse);
-}
-
-/**
- * Handles response by converting it to text and passing the result to
- * addQuoteToDom().
- */
-function handleResponse(response) {
-  console.log('Handling the response.');
-
-  // response.text() returns a Promise, because the response is a stream of
-  // content and not a simple variable.
-  const textPromise = response.text();
-
-  // When the response is converted to text, pass the result into the
-  // addQuoteToDom() function.
-  textPromise.then(addQuoteToDom);
-}
-
-/** Adds a random quote to the DOM. */
-function addQuoteToDom(quote) {
-  console.log('Adding quote to dom: ' + quote);
-
-  const quoteContainer = document.getElementById('quote-container');
-  quoteContainer.innerText = quote;
-}
-
-/**
  * The above code is organized to show each individual step, but we can use an
  * ES6 feature called arrow functions to shorten the code. This function
  * combines all of the above code into a single Promise chain. You can use
@@ -77,54 +40,44 @@ function getRandomQuoteUsingArrowFunctions() {
 }
 
 function getComment() {
-    fetch('/data').then(response => response.text()).then((comment_author) => {
+    fetch('/data').then(response => response.json()).then((comment_author) => {
     document.getElementById('comment_author').innerText = comment_author;
   });
-  fetch('/data').then(response => response.text()).then((comment) => {
+  fetch('/data').then(response => response.json()).then((comment) => {
     document.getElementById('text-input').innerText = comment;
   });
 }
 
 /**
- * Another way to use fetch is by using the async and await keywords. This
- * allows you to use the return values directly instead of going through
- * Promises.
+ * Fetches quotes from the servers and adds them to the DOM.
  */
-async function getRandomQuoteUsingAsyncAwait() {
-  const response = await fetch('/data');
-  const quote = await response.text();
-  document.getElementById('comment_author').innerText = comment_author;
-  document.getElementById('text-input').innerText = comment;
-  //document.getElementById('quote-container').innerText = quote;
+
+function getQuotesFromServer() {
+//   fetch('/data').then(response => response.json()).then((quotesToGson) => {
+//     const statsListElement = document.getElementById('quote-container');
+//     statsListElement.innerHTML = '';
+//     statsListElement.appendChild(
+//         createListElement('First Quote: ' + quotesToGson[0]));
+//     statsListElement.appendChild(
+//         createListElement('Second Quote: ' + quotesToGson[1]));
+//     statsListElement.appendChild(
+//         createListElement('Third Quote: ' + quotesToGson[2]));
+//   });
+  const serverQuotes = ["I am in a charming state of confusion. - Ada Lovelace","It is much easier to apologise than it is to get permission. - Grace Hopper", "You can do anything you want to, but you have to work at it - Annie Easley"]
+  const quoteContainer = document.getElementById('quote-container');
+  quoteContainer.innerHTML = '';
+    quoteContainer.appendChild(
+        createListElement('First Quote: ' + serverQuotes[0]));
+    quoteContainer.appendChild(
+        createListElement('Second Quote: ' + serverQuotes[1]));
+    quoteContainer.appendChild(
+        createListElement('Third Quote: ' + serverQuotes[2]));
 }
 
-/**
- * Fetches quotess from the servers and adds them to the DOM.
- */
-/*function getQuotesFromServer() {
-  fetch('/data').then(response => response.json()).then((quotes) => {
-    // stats is an object, not a string, so we have to
-    // reference its fields to create HTML content
+/** Creates an <p> element containing text. */
 
-    const statsListElement = document.getElementById('quote-container');
-    statsListElement.innerHTML = '';
-    statsListElement.appendChild(
-        createListElement('First Quote: ' + quotes.firstQuote));
-    statsListElement.appendChild(
-        createListElement('Second Quote: ' + quotes.secondQuote));
-    statsListElement.appendChild(
-        createListElement('Third Quote: ' + quotes.thirdQuote));
-    statsListElement.appendChild(
-        createListElement('Fourth Quote: ' + quotes.fourthQuote));
-    statsListElement.appendChild(
-        createListElement('Fifth Quote: ' + quotes.fifthQuote));
-  });
-}*/
-
-/** Creates an <li> element containing text. */
-/*function createListElement(text) {
-  const liElement = document.createElement('li');
-  liElement.innerText = text;
-  return liElement;
-}*/
-
+function createListElement(text) {
+  const pElement = document.createElement('p');
+  pElement.innerText = text;
+  return pElement;
+}
